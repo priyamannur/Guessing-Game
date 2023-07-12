11cdfa675f489255933c4efcc861d7d4696bcc4e
Priya Mannur <101945448+priyamannur@users.noreply.github.com>
2023-07-12 20:31:04 +0000

    Modified

--- Guessing.py
+++ Guessing.py
@@ -1,6 +1,4 @@
-#CODE HERE
 from random import Random, randint
-
 
 start=int(input("Enter the range of the numbers\nfrom : "))
 end=int(input("to: "))
@@ -8,10 +6,14 @@ val=randint(start,end)
 flag=1
 while flag!=0 :
     guess=int(input("Enter the value :"))
-    if guess < val :
+    if (guess - val) <= -15 :
         print("Try again !! The guess made is too low")
-    elif guess > val :
+    elif (guess < val):
+        print("CLose enough ,Try again!! The guess made is low")
+    elif (guess - val) >= 15 :
         print("Try again !! The guess made is too high")
-    else :
-        print("The guess made is correct to the system selected random val",val)
+    elif (guess > val):
+        print("CLose enough ,Try again!! The guess made is high")
+    elif (guess - val)==0  :
+        print("Woohoooo!! The guess made is correct! It matches the value chose by the system:",val)
         flag-=1
