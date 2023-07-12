5d625933b7ff7b48feb195604b340b74ceafef3d
samruddhishinde20003 <104639087+samruddhishinde20003@users.noreply.github.com>
2023-07-13 00:58:56 +0530

    Update Guessing.py

--- Guessing.py
+++ Guessing.py
@@ -1,1 +1,17 @@
 #CODE HERE
+from random import Random, randint
+
+
+start=int(input("Enter the range of the numbers\nfrom : "))
+end=int(input("to: "))
+val=randint(start,end)
+flag=1
+while flag!=0 :
+    guess=int(input("Enter the value :"))
+    if guess < val :
+        print("Try again !! The guess made is too low")
+    elif guess > val :
+        print("Try again !! The guess made is too high")
+    else :
+        print("The guess made is correct to the system selected random val",val)
+        flag-=1
