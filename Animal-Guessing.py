7039e8a7547d5115f754a5a4a5716f332493068c
Priya Mannur <priyamannur14608@gmail.com>
2023-07-14 01:00:43 +0530

    Modified Animal-Guessing game

--- Animal-Guessing.py
+++ Animal-Guessing.py
@@ -1,13 +1,14 @@
 import random
-import string
-animal_list = ["lion" ,"tiger" ,"hippopotomus"]
-chose_by_system = random.choice(animal_list)
+chose_by_system = random.choice(open("animals.txt","r").read().split())
 flag = 1
+print("The Animal Guessing Game starts here!!")
 while flag!=0:
-    print("The Animal Guessing Game starts here!!")
     chose_by_user = input("Enter the animal: ")
     if chose_by_system==chose_by_user:
         print("Yay!! you got it right!")
         flag = 0
     else:
+        with open("animals.txt","a") as file:
+            file.write(chose_by_user)
         print("Sorry!! try again")
+
