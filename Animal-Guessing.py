d288d5e4bd26c26dabb6cb182c0b42c3ac91417e
Priya Mannur <priyamannur14608@gmail.com>
2023-07-14 21:50:04 +0530

    Modified Animal-Guessing game

--- Animal-Guessing.py
+++ Animal-Guessing.py
@@ -1,14 +1,38 @@
-import random
-chose_by_system = random.choice(open("animals.txt","r").read().split())
+from random import Random, randint
+my_dict = {
+    'tiger' : 'Striped animal, orange',
+    'lion' : 'King',
+    'Camel' : 'Dessert',
+    'Sam' : 'My best friend',
+    'Cow' : 'Sacared one',
+    'goat' : 'bakara , another name of sam fyi',
+    'ictoae' : 'I cannot think of anything else'
+        }
+end = len(my_dict)
+i = randint(0,end-1)
+keys = list(my_dict.keys())
+chose_by_system = keys[i]
 flag = 1
+count = 0
+hint = 0
 print("The Animal Guessing Game starts here!!")
 while flag!=0:
+    if hint == 1:
+        value= list(my_dict.values())
+        print(value[i])
+        hint = 0
     chose_by_user = input("Enter the animal: ")
     if chose_by_system==chose_by_user:
         print("Yay!! you got it right!")
         flag = 0
     else:
-        with open("animals.txt","a") as file:
-            file.write(chose_by_user)
         print("Sorry!! try again")
+    count = count + 1
+    if count >= 3 and flag!=0:
+        get= input("DO you want a hint? ,press y/n:")
+        if get == 'y' or get == 'Y':
+            hint = 1
+        else :
+            hint = 0
+    
 
